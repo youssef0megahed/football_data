bbe55c02bf60cade7f04eedd214f63e8a173695e
youssef0megahed <youssef0megahed@gmail.com>
2026-08-18 02:59:10 +0300

    Update Supabase key environment variable

--- src/test.py
+++ src/test.py
@@ -14,7 +14,7 @@ from supabase import create_client, Client
 # ============================================================
 
 SUPABASE_URL = os.environ["SUPABASE_URL"]
-SUPABASE_KEY = os.environ["SUPABASE_SERVICE_ROLE_KEY"]
+SUPABASE_KEY = os.environ["SUPABASE_KEY"]
 
 supabase: Client = create_client(
     SUPABASE_URL,
